2db401eb4637cbaed28551e1c3c8bffe32eb919f
Roland Tamas <rolandtamas98@gmail.com>
2021-04-22 22:11:58 +0300

    merged conflicting files

--- Slave/src/Slave.c
+++ Slave/src/Slave.c
@@ -3,11 +3,8 @@
  *****************************************************************************/
 
 #include <sys/platform.h>
-<<<<<<< HEAD
 #include <defBF537.h>
-=======
 #include <cdefBF537.h>
->>>>>>> 5b49b33e1bf08c8e0f1664c0def4ccafc179f59d
 #include <ccblkfn.h>
 #include "adi_initialize.h"
 #include "Slave.h"
@@ -17,7 +14,6 @@
  */
 char __argv_string[] = "";
 
-<<<<<<< HEAD
 void spiInitialization()
 {
 	uint16_t controlRegister = SPI_CTL;
@@ -32,8 +28,7 @@ void spiInitialization()
 	controlRegister |= 0x1000;
 }
 
-=======
->>>>>>> 5b49b33e1bf08c8e0f1664c0def4ccafc179f59d
+
 int main(int argc, char *argv[])
 {
 	/**
@@ -42,27 +37,9 @@ int main(int argc, char *argv[])
 	 * @return zero on success 
 	 */
 	adi_initComponents();
-<<<<<<< HEAD
 	spiInitialization();
-=======
-	
 
->>>>>>> 5b49b33e1bf08c8e0f1664c0def4ccafc179f59d
 
 	return 0;
 }
 
-<<<<<<< HEAD
-=======
-void SPIInitialization()
-{
-	//set PF11 as input pin
-	*pPORTFIO_INEN = 0x0800;
-
-	//enable this device as a slave
-	SPI_CTL = 0x0010;
-
-
-}
-
->>>>>>> 5b49b33e1bf08c8e0f1664c0def4ccafc179f59d
--- Slave/src/Slave.h
+++ Slave/src/Slave.h
@@ -5,7 +5,6 @@
 #ifndef __SLAVE_H__
 #define __SLAVE_H__
 
-<<<<<<< HEAD
 typedef struct SPI_DATA
 {
 	uint16_t data;
@@ -13,8 +12,5 @@ typedef struct SPI_DATA
 }SPI_DATA;
 
 SPI_DATA data[1000];
-=======
-/* Add your custom header content here */
->>>>>>> 5b49b33e1bf08c8e0f1664c0def4ccafc179f59d
 
 #endif /* __SLAVE_H__ */
