545b2fb6db0be5f915b96dfc55821c56f5e0969b
Roland Tamas <rolandtamas98@gmail.com>
2021-05-05 13:04:50 +0300

    Modified Slave.c

--- Slave/src/Slave.c
+++ Slave/src/Slave.c
@@ -9,6 +9,33 @@
 #include "adi_initialize.h"
 #include "Slave.h"
 
+//signals used in SPI transfers
+
+#define MOSI (1<<11)
+#define MISO (1<<12)
+#define SCK  (1<<13)
+#define _SPISS (1<<14)
+
+//build the SPI_CTL register
+
+/*#define SPI_ENABLE (1<<14)
+#define WOM (0<<13)
+#define SLAVE_SET (0<<12)
+#define CPOL (0<<11)
+#define CPHA (0<<10)
+#define LSBF (1<<9)
+#define WORD_SIZE (1<<8)
+#define EMISO (0<<5)
+#define PSSE (1<<4)
+#define GM (1<<3)
+#define SZ (1<<2)
+#define TIMOD1 (0<<1)
+#define TIMOD0 0*/
+#define SLAVE_SET (0<<12)
+#define WORD_SIZE (1<<8)
+#define TIMOD1 (0<<1)
+#define TIMOD0 0
+
 /** 
  * If you want to use command program arguments, then place them in the following string. 
  */
@@ -17,29 +44,32 @@ char __argv_string[] = "";
 void spiInitialization()
 {
 
-	// Enable PF11(MOSI) & PF13(SCK) as input
-	*pPORTFIO_INEN = (1<<11) | (1<<13);
+	// Enable PF11(MOSI) & PF13(SCK) as input,
+	// Enable PF12(MISO) as output
+	// SCK is enabled as input because
+	// it receives the clock signal from
+	// the master(they should be in sync)
 
+	*pPORTFIO_INEN = MOSI | MISO | SCK;
 
-	// Initialize device as slave device,
-	// by activating bit 12 of the
-	// SPI_CTL Register
+	// Select this device as a slave
 
-	*pSPI_CTL |= (1<<12);
+	*pPORTF_FER |= _SPISS;
 
-	// also set TIMOD to 00,
-	// so that it starts transfer with a read
-	// of SPI_RDBR (an interrupt is issued
-	// when RDBR is full)
-	*pSPI_CTL &= ~(1<<0);
+	// Set the bits from the CTL register
+
+	*pSPI_CTL = SPE | WOM | SLAVE_SET | CPOL | CPHA | LSBF | WORD_SIZE | EMISO | PSSE |
+			GM | SZ | TIMOD1 | TIMOD0;
 }
 
 void readData()
 {
 	int i=0;
 
-	while(*pSPI_CTL==0x0000)
-	{
+	while(*pSPI_STAT!=0x0001)
+	{// While bit 1 from SPI_STAT is not set to 1,
+		// which indicates a successful word transfer,
+		// continue reading data from SPI_RDBR
 		data[i].data = *pSPI_RDBR;
 		i++;
 	}
